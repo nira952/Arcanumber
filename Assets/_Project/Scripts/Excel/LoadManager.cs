5af89ebe503aa0f69fa5cd983cd8e91b0860a0b2
NoCheL <wateracqua0522@gmail.com>
2026-07-31 13:29:35 +0900

    変更：LoadManager

--- Assets/_Project/Scripts/Excel/LoadManager.cs
+++ Assets/_Project/Scripts/Excel/LoadManager.cs
@@ -7,15 +7,16 @@ using UnityEngine;
 
 public class LoadManager : SingletonMonoBehaviour<LoadManager>
 {
+
     //アルカナデータ
-    private string arcanaFilePath = Application.dataPath + "/_Project/Excel/ArcanaData.xlsx";
+    private string arcanaFilePath => GetExcelPath("ArcanaData.xlsx");
     private int arcanaSheetNumber = 1;    //シートの番号
     private IWorkbook arcanaWorkbook; //Excelファイル
     private ISheet arcanaSheet;   //シート
     private IFormulaEvaluator arcanaEvaluator;    //計算機
 
     //スキルデータ
-    private string skillFilePath = Application.dataPath + "/_Project/Excel/SkillData.xltm";
+    private string skillFilePath => GetExcelPath("SkillData.xltm");
     private int skillSheetNumber = 0;    //シートの番号
     private IWorkbook skillWorkbook; //Excelファイル
     private ISheet skillSheet;   //シート
@@ -24,6 +25,16 @@ public class LoadManager : SingletonMonoBehaviour<LoadManager>
     //データ保管
     private List<Arcana> arcanaList = new List<Arcana>();
     private List<Skill> skillList = new List<Skill>();
+
+    private string GetExcelPath(string fileName)
+    {
+        if (Application.isEditor)
+            //Unityエディタで再生中は、現在の開発用フォルダを見る
+            return Path.Combine(Application.dataPath, "_Project/Excel", fileName);
+        else
+            //ビルド後では、StreamingAssetsフォルダを見る
+            return Path.Combine(Application.streamingAssetsPath, fileName);
+    }
 
     public void Initialize() 
     {
