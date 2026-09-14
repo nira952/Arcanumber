61788736f9a26a2606c6139350157f5343752408
NoCheL <wateracqua0522@gmail.com>
2026-09-14 11:12:51 +0900

    BGM,SEのセット

--- Assets/Plugins/AudioManager/AudioSetting.cs
+++ Assets/Plugins/AudioManager/AudioSetting.cs
@@ -9,14 +9,41 @@ public enum BgmName
 {
     Title,
     Game,
+    Tutorial,
+    Practice,
+    Gameend,
+    Blue,
+    Red,
+    Green,
+    Yellow,
     num
 }
 
 // 必要なSEを入力
 public enum SeName
 {
+    Select,
+    Click,
+    Walk,
+    Jump,
+    SetGround,
     Damege,
     Attack,
+    Thunder,
+    Ice,
+    Shadow,
+    Poison,
+    Heal,
+    Buff,
+    Debuff,
+    Guard,
+    Warp,
+    TimeStop,
+    Stan,
+    Purification,
+    Alert,
+    Spear,
+    Summon,
     Button,
     num,
 }
